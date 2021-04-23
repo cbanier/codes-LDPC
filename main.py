8981a9c9c79e0b62bdb8b9c83384381ce79bf802
Corentin Banier <corentin.banier@etu.u-bordeaux.fr>
2021-04-23 22:25:02 +0200

    Modification de la fonction de décodage LDPC strict

--- main.py
+++ main.py
@@ -51,9 +51,15 @@ def decode_LDPC_aux_strict(H,S,weight_S,n,break_cpt):
     if break_cpt >= 25:
         return []
     L = []
-    for i in range(n):
-        if weightOfCol(mod2(S + getCol(H,i), n//2),n//2) < weight_S:
-            L.append(i)
+    # On essaie de supprimer les positions parasites dès le début
+    if break_cpt == 1:
+        for i in range(n):
+            if weightOfCol(mod2(S + getCol(H,i), n//2),n//2) <= weight_S - 5:
+                L.append(i)
+    else:
+        for i in range(n):
+            if weightOfCol(mod2(S + getCol(H,i), n//2),n//2) <= weight_S:
+                L.append(i)
     S2 = np.zeros(n//2)
     for i in L:
         S2 = S2 + getCol(H,i)
@@ -63,7 +69,7 @@ def decode_LDPC_aux_strict(H,S,weight_S,n,break_cpt):
     if weightOfCol(S3,n//2) == 0:
         return L
     else:
-        return decode_LDPC_aux(H,S3,weightOfCol(S3,n//2),n,break_cpt+1)
+        return decode_LDPC_aux_strict(H,S3,weightOfCol(S3,n//2),n,break_cpt+1)
 
 def decode_LDPC_strict(H,e,n):
     S = mod2(H@(e.T),n//2)
@@ -107,46 +113,48 @@ def displayTest(res,weight_e,max_loop):
 ########################################################
 
 def opt_weight_search(max_loop,n):
-    w = 1
-    for j in range(25):
+    for j in range(5,25):
         start = time()
-        H = matrixFromWeight(w+j,n)
+        H = matrixFromWeight(j,n)
         print(f"Soit H une matrice de taille {n//2}x{n}")
-        print(f"Dont le poids des colonnes est {w+j}.\n")
+        print(f"Dont le poids des colonnes est {j}.\n")
         i = 1
         # Arrêt lorsque la proba est inférieur à 0.4
+        # Ou si la proba est différente de 1 pour le poids 1
+        # (on gagne du temps, puisque'on sait que c'est pas optimal)
         tmp = test_decode_LDPC(H,i,max_loop,n)
-        while tmp >= 0.5:
+        if tmp == 1:
+            while tmp >= 0.67:
+                displayTest(tmp,i,max_loop)
+                i += 1
+                tmp = test_decode_LDPC(H,i,max_loop,n)
             displayTest(tmp,i,max_loop)
-            i += 1
-            tmp = test_decode_LDPC(H,i,max_loop,n)
-        displayTest(tmp,i,max_loop)
-        print("Time:",time() - start, " secondes \n")
+            print("Time:",time() - start, " secondes \n")
+        else:
+            displayTest(tmp,i,max_loop)
+            print("Time:",time() - start, " secondes \n")
     return None
 
 # Lorsqu'on a trouvé le poids optimal pour le décodage d'une matrice
 # de taille (n//2,n) on regarde si on peut améliorer le décodage.
 
 def opt_weight_search_strict(borne_inf,borne_sup,max_loop,n):
-    w , max_ite = 0 , 3
     print("Version optimal")
     for j in range(borne_inf,borne_sup):
-        for i in range(max_ite):
-            print(f"{i+1} itération(s):")
-            start = time()
-            H = matrixFromWeight(w+j,n)
-            print(f"Soit H une matrice de taille {n//2}x{n}")
-            print(f"Dont le poids des colonnes est {w+j}.\n")
-            i = 1
-            tmp = test_decode_LDPC_strict(H,i,max_loop,n)
-            while tmp >= 0.5:
-                displayTest(tmp,i,max_loop)
-                i += 1
-                tmp = test_decode_LDPC_strict(H,i,max_loop,n)
+        start = time()
+        H = matrixFromWeight(j,n)
+        print(f"Soit H une matrice de taille {n//2}x{n}")
+        print(f"Dont le poids des colonnes est {j}.\n")
+        i = 1
+        tmp = test_decode_LDPC_strict(H,i,max_loop,n)
+        while tmp >= 0.67:
             displayTest(tmp,i,max_loop)
-            print("Time:",time() - start, " secondes \n")
+            i += 1
+            tmp = test_decode_LDPC_strict(H,i,max_loop,n)
+        displayTest(tmp,i,max_loop)
+        print("Time:",time() - start, " secondes \n")
     return None
 
 if __name__ == "__main__":
-    opt_weight_search(25,1000)
-    opt_weight_search_strict(7,15,25,1000)
+    #opt_weight_search(27,2000)
+    opt_weight_search_strict(13,21,50,2000)
